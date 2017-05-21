f71bdb6e885df738e9ae3f79a385952cc674b332
blaecwen <blaecwen@gmail.com>
2017-05-21 15:07:47 +0300

    Add text to help message

--- rclogd/main.cpp
+++ rclogd/main.cpp
@@ -9,12 +9,18 @@
 
 namespace po = boost::program_options;
 
-// TODO: dbms should be one of constants
+// TODO: dbms should be one of constants. Maybe could be done using notify!
 
 const char help_message[] = "rclogd -- service managing logs on robotic node system. "
                             "It is a part of distributed robotic log delivery system\n\n"
                             "Usage:\n"
-                            "    rclogd [OPTIONS...]";
+                            "    rclogd [OPTIONS...]\n\n"
+                            "All options in \"Configuration\" section can also be specified "
+                            "in config file defined by --config option.\n"
+                            "Option specified both in config file and command line arguments "
+                            "will have value specified in command line.\n"
+                            "Also, it should be noted, that options without arguments (e.g. --socket-takeover) "
+                            "should be specified with True/False argument when defined in config file.";
 
 const char version_message[] = "Distributed robotic log delivery system, Dmitry Liman, 2017, version 0.1";
 
@@ -27,7 +33,7 @@ int readOptions(int argc, char* argv[], po::variables_map &vm) {
     generic.add_options()
         ("version,v", "print version string")
         ("help,h", "produce help message")
-        ("config,c", po::value<std::string>(&config_file),
+        ("config,c", po::value<std::string>(&config_file)->value_name("filename"),
               "name of a configuration file");
 
     // options allowed both on command line and in config file
@@ -35,11 +41,14 @@ int readOptions(int argc, char* argv[], po::variables_map &vm) {
     config.add_options()
         ("socket,s", po::value<std::string>()->required(),
               "Path where unix socket will be created\n")
+        ("socket-takeover",
+              po::value<bool>()->implicit_value(true, "")->default_value(false, "")->value_name(" "),
+              "Takes over socket file if it is already exists. The default is false\n")
         ("dbms,D", po::value<std::string>()->required(),
               "DBMS to use as local storage system for logs\n")
         ("id", po::value<std::string>()->required(),
               "ID of robotic node\n")
-        ("priority", po::value<int>()->required(),
+        ("priority,p", po::value<int>()->required(),
               "Priority of robotic node.\nUsed by nodes to deside who should be master in PtP connection\n");
 
     // group all possible options
@@ -97,12 +106,13 @@ int main(int argc, char* argv[]) {
             dbManager = std::make_shared<rclog::MongoManager>();
         }
         else {
-            std::cerr << "Unknown database, valid values: mongodb\n";
+            std::cerr << "Unknown database specified, valid values: mongodb\n";
             return 1;
         }
 
         std::string socket = vm["socket"].as<std::string>();
-        ::unlink(socket.c_str());     // NOTE: is it correct behavior?
+        if(vm["socket-takeover"].as<bool>())
+            ::unlink(socket.c_str());     // NOTE: is it correct behavior?
         boost::asio::io_service io_service;
         rclog::LocalServer s(io_service, socket, *dbManager);
         io_service.run();
